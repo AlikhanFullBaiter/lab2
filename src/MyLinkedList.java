6aea32d1580e5f027ec11cce889f45995e2e885f
Alikhan <astanaproteam123@gmail.com>
2023-04-19 14:05:59 +0600

    adding remove methods to MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -82,14 +82,70 @@ public class MyLinkedList<E> implements MyList<E> {
     }
 
     @Override
-    public boolean remove(E item) {
+    public boolean remove(Object item) {
+        Node<E> newNode = new Node<E>((E) item, null, null);
+        if (head.element == newNode.element) {
+            head = head.next;
+            head.prev = null;
+            size--;
+            return true;
+        }
+        Node<E> node = head.next;
+        while (node != null) {
+            if (node.element == newNode.element) {
+                Node<E> temp = node.prev;
+                temp.next = node.next;
+                Node<E> temp2 = node.next;
+                temp2.prev = temp;
+                size--;
+                return true;
+            }
+            node = node.next;
+        }
         return false;
     }
 
     @Override
     public E remove(int index) {
+        checkIndex(index);
+        if (index == 0) {
+            E delete;
+            delete  = head.element;
+            if (size == 1) {
+                head = null;
+                tail = null;
+                size = 0;
+                return delete;
+            }
+            head = head.next;
+            head.prev = null;
+            size--;
+            return delete;
+        }
+        if (index == size-1) {
+            E delete = tail.element;
+            tail = tail.prev;
+            tail.next = null;
+            size--;
+            return delete;
+        }
+        Node<E> node = head.next;
+        for (int i = 1; i <= size; i++) {
+            if (index == i) {
+                Node<E> p = node.prev;
+                Node<E> n = node.next;
+                E delete = node.element;
+
+                p.next = n;
+                n.prev = p;
+                size--;
+                return delete;
+            }
+            node = node.next;
+        }
         return null;
     }
+
 
     @Override
     public void clear() {
